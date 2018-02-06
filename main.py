61a5afc0acfe93a170deba542fa4143acc9efdfb
samarth_Ag <samarth.ag1234@gmail.com>
2018-02-06 10:12:13 -0800

    user info and post

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import urllib
 
 APP_ACCESS_TOKEN='4870715640.a48e759.874aba351e5147eca8a9d36b9688f494'
 BASE_URL='https://api.instagram.com/v1/'
@@ -11,13 +12,90 @@ def self_info():
     user_info = requests.get(request_url).json()
 
     if user_info['meta']['code'] == 200:
-        print "Username : "+user_info['data']['username']
-        print "Full Name : "+ user_info['data']['full_name']
-        print "No. of followers %s" %(user_info['data']['counts']['followed_by'])
-        print "No. of people following %s" % (user_info['data']['counts']['follows'])
-        print "Total posts %s" %(user_info['data']['counts']['media'])
+        if 'data' in user_info:
+            print 'Username: %s' % (user_info['data']['username'])
+            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'User does not exist!'
     else:
         print 'Status code other than 200 received!'
 
 
 self_info()
+
+
+def get_own_post():
+  request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (APP_ACCESS_TOKEN)
+  print 'GET request url : %s' % (request_url)
+  own_media = requests.get(request_url).json()
+  if own_media['meta']['code'] == 200:
+      if len(own_media['data']) > 0:
+          image_name = own_media['data'][0]['id'] + '.jpeg'
+          image_url = own_media['data'][0]['images']['standard_resolution']['url']
+          urllib.urlretrieve(image_url, image_name)
+          print 'Your image has been downloaded!'
+      else:
+          print 'Post does not exist!'
+  else:
+     print 'Status code other than 200 received!'
+
+
+
+def get_user_id(insta_username):
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            return user_info['data'][0]['id']
+        else:
+            return None
+    else:
+        print 'Status code other than 200 received!'
+        exit()
+
+
+def get_user_info(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if 'data' in user_info:
+            print 'Username: %s' % (user_info['data']['username'])
+            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'There is no data for this user!'
+    else:
+        print 'Status code other than 200 received!'
+
+
+def get_user_post(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']) > 0:
+            image_name = user_media['data'][0]['id'] + '.jpeg'
+            image_url = user_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print 'Your image has been downloaded!'
+        else:
+            print 'Post does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+
+get_user_post("rajat8310")
